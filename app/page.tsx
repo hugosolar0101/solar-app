dfc39795f6d2a11446dafb6db60d20e9d32714d7
Hugo <hugo.solar0101@gmail.com>
2026-04-15 14:01:07 -0300

    fix redirecionamento login v2.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
 
 export default function Home() {
   redirect("/login");
-  }
+}
